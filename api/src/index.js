b95c91aa468b322ab0a4eb38490087ce5fb04c46
sebtcheng <sebastianocheng@gmail.com>
2026-06-24 10:17:58 +0800

    mod-prevent login user for resign

--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,7 @@ import thirdLevelRoutes from './routes/thirdLevelRoutes.js';
 import binaryRoutes from './routes/binaryRoutes.js';
 import { initDB } from './config/db.js';
 import pool from './config/db.js';
+import compression from 'compression';
 
 const app = express();
 const port = process.env.THIRD_LEVEL_PORT || process.env.PORT || 3008;
@@ -20,6 +21,7 @@ process.on('uncaughtException', (err) => {
 initDB();
 
 app.use(cors());
+app.use(compression());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
